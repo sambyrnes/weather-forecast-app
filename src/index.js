0105f099f42aff8af68bbaf8e6a719028f5df794
sambyrnes <sambyrnes13@gmail.com>
2024-10-30 21:15:57 -0500

    Made updates to HTML, CSS, and JS files
    Updated HTML file title to reflect app functionality more accurately (building out a forecast feature will come later). Added a conditional if statement of the hours in the time element. Removed the emoji and created a new div contained with the id of "icon." Restyled elements to reposition them aesthetically. Added two lines, where the first line is selecting the icon element that has nothing in it and then injecting the image
    using the URL based on the API.

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
   descriptionElement.innerHTML = response.data.condition.description;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}m/s`;
   temperatureElement.innerHTML = Math.round(temperature);
 }
 
@@ -32,6 +34,10 @@ function formatDate(date) {
 
   if (minutes < 10) {
     minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
   }
 
   return `${day} ${hours}:${minutes}`;
