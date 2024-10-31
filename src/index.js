d016ccace3cf64a8ee70dffedb662af5c432b4ff
sambyrnes <sambyrnes13@gmail.com>
2024-10-30 20:09:57 -0500

    Added search functionality to JS file using Weather API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-function searchCity(event) {
+function refreshWeather(response) {
+  let temperatureElement = document.querySelector("#temperature-value");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = response.data.city; // innerHTML: Replaces h1 with a user's inputed city
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  // Why do we create separate function? Separation of concerns
+  // Make API call and update the interface
+  let apiKey = "76c81b0fc1ef1c3538e03a284oa1t4b1";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function activateSearchFunction(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML =
-    searchInput.value; /* This replaces the h1 tag with a user's inputed city */
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", searchCity);
+searchFormElement.addEventListener("submit", activateSearchFunction);
+
+searchCity("Seattle"); // Shows Seattle by default until user inputs something else
