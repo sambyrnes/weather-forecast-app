4802d1fb8e4b1d7b4865b3a551ee4bb43f0c59b5
sambyrnes <sambyrnes13@gmail.com>
2024-11-02 15:56:14 -0500

    Updated JS file: replaced dummy data with real-time data from API
    Updated the CSS file for improved readability. Changed the JS file to format the day and replaced dummy data with real-time data from the provided Weather API in the displayForecast function.

--- src/index.js
+++ src/index.js
@@ -60,6 +60,13 @@ function activateSearchFunction(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "76c81b0fc1ef1c3538e03a284oa1t4b1";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -69,24 +76,27 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
       <div class="weather-forecast-day">
-        <div class="weather-forecase-date">${day}</div>
-          <div class="weather-forecast-icon">☀️</div>
+        <div class="weather-forecase-date">${formatDay(day.time)}</div>
+          <img src="${day.condition.icon_url}" class="weather-forecast-icon" /> 
           <div class="weather-forecast-temperatures">
             <div class="weather-forecast-temperature">
-              <strong>15°</strong>
+              <strong>${Math.round(day.temperature.maximum)}°</strong>
             </div>
-          <div class="weather-forecast-temperature">9°</div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}°</div>
         </div>
       </div>    
     `;
+    }
   });
 
   let forecastElement = document.querySelector("#forecast");
